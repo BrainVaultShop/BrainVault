254639bfe4e6fa238d46044b4db5c6cc5dc96575
BrainVaultShop <brainvault.shop@gmail.com>
2025-08-05 18:06:01 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function checkout(item) {
-    alert("Simulación de compra: " + item + "\nEn versión real: se abrirá el checkout seguro de Stripe.");
+    alert("Simulación de compra: " + item + "\nEn producción: aquí abriría Stripe Checkout.");
 }
